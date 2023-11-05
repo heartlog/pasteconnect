75600dd29c5b6a1e41877f2d9df9a2950f6ea673
Maday <76758776+heartlog@users.noreply.github.com>
2023-11-05 11:05:10 +0530

    update pasteconnect/reqsession.py

--- pasteconnect/reqsession.py
+++ pasteconnect/reqsession.py
@@ -5,8 +5,6 @@ def reqsession(url, params=None):
     session = requests.Session()
     session.headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.1.2222.33 Safari/537.36",
-        "Accept-Encoding": "*",
-        "Connection": "keep-alive"
     }
     try:
         response = session.get(url, params=params)
