a9711e8c4b9e8eaee58660e66d469c5b17264668
Maday <76758776+heartlog@users.noreply.github.com>
2023-11-05 11:12:51 +0530

    update pasteconnect/reqsession.py

--- pasteconnect/reqsession.py
+++ pasteconnect/reqsession.py
@@ -28,3 +28,33 @@ def Advreqsession(url, params=None):
         print(f"An error occurred: {e}")
     finally:
         session.close()
+        
+def postsession(url, data, files=None):
+    session = requests.Session()
+    session.headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.1.2222.33 Safari/537.36",
+        "Accept-Encoding": "*",
+        "Connection": "keep-alive"
+    }
+    try:
+        response = session.post(url, data=data, files=files)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        session.close()
+    return response
+
+def Advpostsession(url, data=None, files=None):
+    session = requests.Session()
+    ua = UserAgent()
+    session.headers = {
+        "User-Agent": ua.random,
+        "Accept-Encoding": "*"
+    }
+    try:
+        response = session.post(url, data=data, files=files)
+        return response
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        session.close()
